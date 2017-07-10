97b3a9c87241854c2a481e58740599be433663b8
Sai Nithish <sainithishgvs@gmail.com>
2017-07-10 09:33:46 -0400

    jul10

--- src/org/nithish/threads/HeapDumpAnalyze.java
+++ src/org/nithish/threads/HeapDumpAnalyze.java
@@ -4,13 +4,6 @@ import java.util.concurrent.TimeUnit;
 
 public class HeapDumpAnalyze {
 
-	public static void CreateOutOfMemoryError() {
-		char[] array = new char[2000000000];
-		String str = new String(array);
-		StringBuffer sb = new StringBuffer();
-		sb.append(str);
-	}
-
 	public static void main(String[] args) throws InterruptedException {
 		char[] array = new char[2000000000];
 		TimeUnit.MINUTES.sleep(10000000);
